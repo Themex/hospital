90e838f03df05028a7f5886f2ccc177c9d83512e
Themex <themes-money@yandex.ru>
2020-03-22 15:45:12 +0300

    - New icons added - Several fixes

--- src/router/index.js
+++ src/router/index.js
@@ -15,11 +15,7 @@ const routes = [
   {
     path: "/office",
     name: "Office",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Office.vue")
+    component: () => import("../views/Office.vue")
   },
   {
     path: "/news",
